2075c964b94238b08593701f0205fee00a61fe12
Solon <amaral.solon@hotmail.com>
2023-09-12 16:39:18 -0300

    finished project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 // console.log("fala mundo!");
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+
 import './style.css';
 
 const generateButton = document.querySelector('button');
 const password = document.querySelector('h2');
+const copyButton = document.getElementById('copy');
 
 generateButton.addEventListener('click', () => {
   const randomPassword = nanoid();
   password.innerHTML = randomPassword;
+});
+
+copyButton.addEventListener('click', () => {
+  copy(password.innerHTML);
+  // eslint-disable-next-line no-alert
+  alert('Copiado Para Área de Transferência!');
 });
